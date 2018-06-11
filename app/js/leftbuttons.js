b2076849a18fb2b3e1bc4a7540408eda6bad88a9
upwardsw <13517078184@139.com>
2018-06-11 11:29:27 +0800

    Initial commit

---
+++ app/js/leftbuttons.js
@@ -0,0 +1,13 @@
+function clickmessagebutton() {
+    var showdiv=document.getElementById("left-bottom");
+}
+window.onload=function clickcontactsbutton() {
+    var showdiv=document.getElementById("left-bottom");
+    showdiv.innerHTML="<div style='cursor: pointer'><div style='font-size: x-large' id='name' name='name' onclick='clicksession(uid)'>name</div>gdgdgfdg</div>"
+}
+function clicksession(id){
+//    change right part:right-title:name,right-bottom-up:message,right-bottom-down:input text and send message
+
+
+
+}
